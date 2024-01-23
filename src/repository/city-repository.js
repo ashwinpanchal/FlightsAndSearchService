0ec8d2e66e222c565638b7a2d4e14bfd396aee50
ashwinpanchal <ashwinpanchal.iitb@gmail.com>
2024-01-23 15:04:46 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,6 +6,7 @@ class CityRepository {
       const city = await City.create({ name });
       return city;
     } catch (error) {
+      console.log("Something went wrong in the repository level");
       throw { error };
     }
   }
@@ -18,6 +19,29 @@ class CityRepository {
         },
       });
     } catch (error) {
+      console.log("Something went wrong in the repository level");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.findOne({ where: { id: cityId } });
+      await city.update(data);
+      await city.save();
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in the repository level");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findOne({ where: { id: cityId } });
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in the repository level");
       throw { error };
     }
   }
